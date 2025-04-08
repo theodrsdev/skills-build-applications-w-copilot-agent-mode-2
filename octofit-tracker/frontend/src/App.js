57657d58e8c07e399b3e0272bbb676491d768450
theodrsdev <tfakiyesi13@gmail.com>
2025-04-08 11:38:03 +0000

    Enhance App.css with colors, add OctoFit logo and favicon

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -6,7 +6,7 @@ import Teams from './components/Teams';
 import Users from './components/Users';
 import Workouts from './components/Workouts';
 
-import logo from './logo.svg';
+import logo from './octofitapp-small.png';
 import './App.css';
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
+            <img src={logo} alt="OctoFit Logo" className="logo" />
             <Link className="navbar-brand" to="/">OctoFit Tracker</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
